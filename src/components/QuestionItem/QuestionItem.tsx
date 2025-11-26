5129414b6c85a1e9d784b22c804489376d27c332
Dmitry <46893211+B1ckbeard@users.noreply.github.com>
2025-11-26 19:03:41 +0300

    added questionAccordeon, questionDetail page

--- src/components/QuestionItem/QuestionItem.tsx
+++ src/components/QuestionItem/QuestionItem.tsx
@@ -1,15 +1,22 @@
 import { Question } from "@/shared/interfaces";
 import styles from "./styles.module.css";
+import { useState } from "react";
+import GradeBadge from "../GradeBadge/GradeBadge";
+import DetailsButton from "../DetailsButton/DetailsButton";
 
 interface Props {
   question: Question;
 }
 
 const QuestionItem = ({ question }: Props) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleToggleAccordeon = () => setIsOpen((prev) => !prev);
+
   return (
-    <div className={styles.item}>
+    <div className={`${styles.item} ${isOpen ? styles.open : ""}`}>
       <div className={styles.titleContent}>
-        <button className={styles.titleButton}>
+        <button className={styles.titleButton} onClick={handleToggleAccordeon}>
           <p className={styles.title}>{question.title}</p>
           <svg
             width="24"
@@ -29,6 +36,16 @@ const QuestionItem = ({ question }: Props) => {
             ></path>
           </svg>
         </button>
+      </div>
+      <div className={styles.accordeon}>
+        <div className={styles.info}>
+          <ul className={styles.gradeList}>
+            <GradeBadge title="Рейтинг:" grade={question.rate} />
+            <GradeBadge title="Сложность:" grade={question.complexity} />
+          </ul>
+          <DetailsButton question={question} />
+        </div>
+        <p>{question.shortAnswer}</p>
       </div>
     </div>
   );
--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -1,4 +1,17 @@
+export interface QuestionSkill {
+  id: number;
+  title: string;
+  description: string;
+}
+
 export interface Question {
   id: number;
   title: string;
+  shortAnswer: string;
+  longAnswer: string;
+  rate: number;
+  complexity: number;
+  description: string;
+  keywords: string[];
+  questionSkills: QuestionSkill[];
 }
