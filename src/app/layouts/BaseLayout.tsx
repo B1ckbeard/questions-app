5698fdfe21673e13f44f282c9564d1e29f396a63
Dmitry <46893211+B1ckbeard@users.noreply.github.com>
2025-12-24 00:17:39 +0300

    Ho-ho-ho

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import styles from "./styles.module.css";
 import { Header } from "@/widgets/header";
 import { Breadcrumbs } from "@/widgets/breadcrumbs";
+import Snowfall from "react-snowfall";
 
 const BaseLayout = () => {
   return (
@@ -9,6 +10,7 @@ const BaseLayout = () => {
       <Header />
       <main className={styles.main}>
         <Breadcrumbs />
+        <Snowfall />
         <Outlet />
       </main>
     </div>
